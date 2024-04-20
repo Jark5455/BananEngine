d0e6250060e0c9199cb7f657d2883ffa097bfe2d
yashr <rshailu.yash@gmail.com>
2024-04-20 14:25:46 -0400

    WyHash Implementation

--- stlext/wyhash.cpp
+++ stlext/wyhash.cpp
@@ -2,6 +2,10 @@
 // Created by yashr on 4/19/24.
 //
 
+// Implementation of WyHash Hashing Algorithm
+
+// Don't ask how this works, I don't know either
+
 #include "wyhash.h"
 
 namespace BananSTLExt {
@@ -46,5 +50,105 @@ namespace BananSTLExt {
     uint64_t BananWyHash::wymix(uint64_t A, uint64_t B) {
         wymum(&A,&B);
         return A^B;
+    }
+
+    uint64_t BananWyHash::wyr8(const uint8_t *p) {
+        uint64_t v;
+        memcpy(&v, p, 8);
+
+        #if WYHASH_LITTLE_ENDIAN
+            return v;
+        #elif defined(_MSC_VER)
+            return _byteswap_uint64(v);
+        #else
+            return __builtin_bswap64(v);
+        #endif
+    }
+
+    uint64_t BananWyHash::wyr4(const uint8_t *p) {
+        uint32_t v;
+        memcpy(&v, p, 4);
+
+        #if WYHASH_LITTLE_ENDIAN
+            return v;
+        #elif defined(_MSC_VER)
+            return _byteswap_ulong(v);
+        #else
+            return __builtin_bswap32(v);
+        #endif
+    }
+
+    uint64_t BananWyHash::_wyr3(const uint8_t *p, size_t k) {
+        return (((uint64_t)p[0]) << 16) | (((uint64_t)p[k >> 1]) << 8) | p[k-1];
+    }
+
+    uint64_t BananWyHash::wyhash(const void *key, size_t len, uint64_t seed, const uint64_t *secret) {
+        const auto *p = (const uint8_t *)key;
+        seed ^= wymix(seed^secret[0],secret[1]);
+        uint64_t a;
+        uint64_t b;
+
+        if (_likely_(len <= 16)) {
+            if (_likely_(len >= 4)) {
+                a = (wyr4(p)<<32) | wyr4(p + ((len >> 3) << 2));
+                b = (wyr4(p + len - 4) << 32) | wyr4(p + len - 4 - ((len >> 3) << 2));
+            } else if (_likely_(len > 0)) {
+                a = _wyr3(p,len);
+                b=0;
+            } else a=b=0;
+        }
+
+        else {
+            size_t i=len;
+            if (_unlikely_(i >= 48)) {
+                uint64_t see1=seed, see2=seed;
+                do {
+                    seed=wymix(wyr8(p)^secret[1],wyr8(p + 8)^seed);
+                    see1=wymix(wyr8(p + 16)^secret[2],wyr8(p + 24)^see1);
+                    see2=wymix(wyr8(p + 32)^secret[3],wyr8(p + 40)^see2);
+                    p += 48;
+                    i -= 48;
+                } while(_likely_(i >= 48));
+                seed ^= see1^see2;
+            }
+
+            while(_unlikely_(i > 16)) {
+                seed = wymix(wyr8(p)^secret[1],wyr8(p + 8)^seed);
+                i -= 16;
+                p += 16;
+            }
+
+            a = wyr8(p + i - 16);  b = wyr8(p + i - 8);
+        }
+
+        a ^= secret[1];
+        b ^= seed;
+        wymum(&a,&b);
+
+        return wymix(a^secret[0]^len,b^secret[1]);
+    }
+
+    uint64_t BananWyHash::wyhash64(uint64_t A, uint64_t B) {
+        A^=0x2d358dccaa6c78a5ull;
+        B^=0x8bb84b93962eacc9ull;
+        wymum(&A,&B);
+
+        return wymix(A^0x2d358dccaa6c78a5ull,B^0x8bb84b93962eacc9ull);
+    }
+
+    uint64_t BananWyHash::wyrand(uint64_t *seed) {
+        *seed+=0x2d358dccaa6c78a5ull;
+
+        return wymix(*seed,*seed^0x8bb84b93962eacc9ull);
+    }
+
+    double BananWyHash::wy2u01(uint64_t r) {
+        const double wynorm = 1.0 / (1ull << 52);
+        return static_cast<double>(r >> 12) * wynorm;
+    }
+
+    double BananWyHash::wy2gau(uint64_t r) {
+        const double wynorm = 1.0 / (1ull << 20);
+        return static_cast<double>((r & 0x1fffff) + ((r >> 21) & 0x1fffff) + ((r >> 42) & 0x1fffff)) * wynorm - 3.0;
     }
 }
--- stlext/wyhash.h
+++ stlext/wyhash.h
@@ -24,10 +24,34 @@
     #define _unlikely_(x) (x)
 #endif
 
+#if defined(_WIN32) || defined(__LITTLE_ENDIAN__) || (defined(__BYTE_ORDER__) && __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__)
+    #define WYHASH_LITTLE_ENDIAN 1
+#elif defined(__BIG_ENDIAN__) || (defined(__BYTE_ORDER__) && __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__)
+    #define WYHASH_LITTLE_ENDIAN 0
+#else
+    #warning could not determine endianness! Falling back to little endian.
+    #define WYHASH_LITTLE_ENDIAN 1
+#endif
+
 namespace BananSTLExt {
     class BananWyHash {
-        static inline uint64_t wyrot(uint64_t x);
-        static inline void wymum(uint64_t *A, uint64_t *B);
-        static inline uint64_t wymix(uint64_t A, uint64_t B);
+        public:
+            static inline uint64_t wyhash(const void *key, size_t len, uint64_t seed, const uint64_t *secret);
+            static inline uint64_t wyhash64(uint64_t A, uint64_t B);
+            static inline uint64_t wyrand(uint64_t *seed);
+            static inline double wy2u01(uint64_t r);
+            static inline double wy2gau(uint64_t r);
+
+        private:
+            static inline uint64_t wyrot(uint64_t x);
+            static inline void wymum(uint64_t *A, uint64_t *B);
+            static inline uint64_t wymix(uint64_t A, uint64_t B);
+
+            static inline uint64_t wyr8(const uint8_t *p);
+            static inline uint64_t wyr4(const uint8_t *p);
+
+            static inline uint64_t _wyr3(const uint8_t *p, size_t k);
+
+            constexpr static const uint64_t wyp[4] = {0x2d358dccaa6c78a5ull, 0x8bb84b93962eacc9ull, 0x4b33a62ed433d4a3ull, 0x4d5a2da51de1aa47ull};
     };
 }
